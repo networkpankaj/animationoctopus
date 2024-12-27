58a894f00e6d6c74f429504dd4341c205eb91f35
pankaj singh koshyari <pankajsinghkoshyari@pankajsinghkoshyari.local>
2024-12-27 10:52:05 +0530

    sixinititalise

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Description />
         {/* <ThirdSection /> */}
         <Contact />
-        <ThirdSection />
+        {/* <ThirdSection /> */}
         {/* <FirstSection /> */}
       </main>
       <ThreeScene />
